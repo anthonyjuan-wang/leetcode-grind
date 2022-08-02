811a2f04d7ca0dd61504b846053285bbcec30fd6
anthony w <aj3wang@uwaterloo.ca>
2022-08-02 12:58:19 -0400

    Update twosum.cpp

--- sigmaleetcodegrind/twosum.cpp
+++ sigmaleetcodegrind/twosum.cpp
@@ -1,1 +1,23 @@
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // Create hash map
+        unordered_map<int,int> hash;
+        // result that we return
+        vector <int> result; 
+        for (int i = 0; i < nums.size(); i++){
+            // if the value already exists 
+            if (hash.count(target - nums[i])){
+             result.push_back (hash[target-nums[i]]);
+             result.push_back (i);
+                return result;
+            }
+            else {
+                // Change the place where the number exists to store the index 
+                hash[nums[i]] = i;
+            }    
+        }
+        return result;
+    }
+};
