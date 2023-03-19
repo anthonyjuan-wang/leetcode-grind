cad67c5545bfe18e4ff09362c5f3840b6453f803
Anthony J. Wang <aj3wang@uwaterloo.ca>
2023-03-19 18:53:08 -0400

    Create maximumSubarray.cpp

---
+++ sigmaleetcodegrind/medium/maximumSubarray.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int curSum = 0;
+        int maxSum = INT_MIN;
+        for (int i = 0; i < nums.size(); i++){
+            curSum = max(nums[i], curSum + nums[i]);
+            maxSum = max(curSum, maxSum);
+        } return maxSum;
+    }
+};
